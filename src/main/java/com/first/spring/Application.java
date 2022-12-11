c998faa0cf75bc01b3da6c7e74ee6976d979faf6
rahilnawab <nawabrahil@gmail.com>
2022-12-11 14:37:42 +0530

    try11

--- src/main/java/com/first/spring/Application.java
+++ src/main/java/com/first/spring/Application.java
@@ -3,7 +3,6 @@ package com.first.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /*
