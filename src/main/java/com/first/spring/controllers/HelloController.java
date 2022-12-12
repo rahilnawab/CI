219c921969f4216987eda5b0c56c40abd4eda103
rahilnawab <nawabrahil@gmail.com>
2022-12-12 13:40:47 +0530

    updated

--- src/main/java/com/first/spring/controllers/HelloController.java
+++ src/main/java/com/first/spring/controllers/HelloController.java
@@ -1,4 +1,4 @@
-package com.first.spring;
+package com.first.spring.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
